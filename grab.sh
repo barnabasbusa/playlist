0a7a271e2257008e06c6e1c8d4726fc514bd1794
Barnabas Busa <busa.barnabas@gmail.com>
2024-01-18 21:27:29 +0100

    change some stuff

--- grab.sh
+++ grab.sh
@@ -7,17 +7,17 @@ VIDEO_LIST_FILE=/home/bbusa/playlist/playlist.txt
 # Change to the repository directory
 cd "$REPO_DIR" || exit
 
-# Perform a git pull
-git pull
+# Check if there are changes (including untracked files)
+if [ "$(git status -uno --porcelain)" ]; then
+    echo "Changes detected. Pulling changes from the remote repository."
 
-# Check if there are changes
-if [ "$(git status --porcelain)" ]; then
-    echo "Changes detected. Running youtube-dl on videos."
+    # Perform a git pull
+    git pull
 
     # Read the video list file and run youtube-dl on each video
     while IFS= read -r video_file; do
         echo "Downloading video: $video_file"
-        youtube-dl "$video_file" -o "/home/bbusa/Videos/%(title)s-%(id)s.%(ext)s" -v -f bestvideo+bestaudio
+        youtube-dl "$video_file"
     done < "$VIDEO_LIST_FILE"
 
     echo "Download complete."
